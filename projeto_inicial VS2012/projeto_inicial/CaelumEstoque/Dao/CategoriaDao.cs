f0f178695f705a51111ad24b702d3da5c6538533
rvazquez <rvf.vazquez@hotmail.com>
2013-12-17 20:13:28 -0200

    adicionando pasta projeto incial vs2012

---
+++ projeto_inicial VS2012/projeto_inicial/CaelumEstoque/Dao/CategoriaDao.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CaelumEstoque.Models;
+using System.Data;
+
+namespace CaelumEstoque.Dao
+{
+    public class CategoriaDao
+    {
+        public List<CategoriaDoProduto> Lista()
+        {
+            List<CategoriaDoProduto> categorias = new List<CategoriaDoProduto>();
+            DBSession session = new DBSession();
+            IDataReader reader = session.CreateQuery("select * from categorias").ExecuteQuery();
+            while (reader.Read())
+            {
+                categorias.Add(new CategoriaDoProduto
+                {
+                    Nome = Convert.ToString(reader["nome"]),
+                    Id = Convert.ToInt32(reader["id"]),
+                    Descricao = Convert.ToString(reader["descricao"])
+                });
+            }
+            reader.Close();
+            return categorias;
+        }
+
+        public void Salva(CategoriaDoProduto categoria)
+        {
+            DBSession session = new DBSession();
+            Query query = session.CreateQuery("insert into categorias (nome, descricao) values (@nome, @descricao)");
+            query.SetParameter("nome", categoria.Nome)
+                 .SetParameter("descricao", categoria.Descricao);
+            query.ExecuteUpdate();
+        }
+
+        public CategoriaDoProduto BuscaPorId(int id)
+        {
+            DBSession session = new DBSession();
+            Query query = session.CreateQuery("select * from categorias where id = @id");
+            query.SetParameter("id", id);
+            IDataReader reader = query.ExecuteQuery();
+            CategoriaDoProduto categoria = null;
+            if (reader.Read())
+            {
+                categoria = new CategoriaDoProduto
+                {
+                    Nome = Convert.ToString(reader["nome"]),
+                    Id = Convert.ToInt32(reader["id"]),
+                    Descricao = Convert.ToString(reader["descricao"])
+                };
+            }
+            return categoria;
+        }
+    }
+}
---
+++ projeto_inicial VS2012/projeto_inicial/CaelumEstoque/Dao/UsuarioDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.Models;
+using System.Data;
+
+namespace CaelumEstoque.Dao
+{
+    public class UsuarioDao
+    {
+        public Usuario Busca(String usuario, String senha)
+        {
+            var session = new DBSession();
+            Query query = session.CreateQuery("select * from usuarios where username=@usuario and senha=@senha");
+            query.SetParameter("usuario", usuario);
+            query.SetParameter("senha", senha);
+            IDataReader reader = query.ExecuteQuery();
+            Usuario user = null;
+            if (reader.Read())
+            {
+                user = new Usuario
+                {
+                    Login = Convert.ToString(reader["username"]),
+                    Senha = Convert.ToString(reader["senha"])
+                };
+            }
+            return user;
+        }
+    }
+}
